cb7acc22b758f145fca33933b465f492dd909e14
Douglas Silva <douglassilvaaraujo78@gmail.com>
2023-11-25 15:15:41 -0300

    refactor: Criando rota para a requisição CreateUser

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -2,9 +2,10 @@ import express from 'express';
 const userRoutes = express.Router();
 
 //Conjunto de requisições do usuário:
-import { findAllUsers } from '../controllers/userControllers';
+import { findAllUsers, createUser } from '../controllers/userControllers';
 
 //Rotas
 userRoutes.get("/", findAllUsers);
+userRoutes.post("/", createUser);
 
 export { userRoutes };
