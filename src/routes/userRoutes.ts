635454980c867ae3c7dd082eef54e91c7c132c84
Douglas Silva <douglassilvaaraujo78@gmail.com>
2023-11-25 19:11:48 -0300

    feat: Criando rota para a requisição de autenticação

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -2,10 +2,11 @@ import express from 'express';
 const userRoutes = express.Router();
 
 //Conjunto de requisições do usuário:
-import { findAllUsers, createUser } from '../controllers/userControllers';
+import { findAllUsers, createUser, authenticateUser } from '../controllers/userControllers';
 
 //Rotas
 userRoutes.get("/", findAllUsers);
 userRoutes.post("/", createUser);
+userRoutes.post("/authentication", authenticateUser);
 
 export { userRoutes };
