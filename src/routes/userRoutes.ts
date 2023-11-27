25ba4c1296326d913f185743b690ca53dfaff7b1
Douglas Silva <douglassilvaaraujo78@gmail.com>
2023-11-27 09:29:06 -0300

    feat: Criando rota para a requisição de edição

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -2,7 +2,7 @@ import express from 'express';
 const userRoutes = express.Router();
 
 //Conjunto de requisições do usuário:
-import { findAllUsers, createUser, authenticateUser, removeUsers } from '../controllers/userControllers';
+import { findAllUsers, createUser, authenticateUser, removeUsers, editUser } from '../controllers/userControllers';
 //Importando o middleware de rerificação de token
 import { verifyToken } from '../middleware/verifyToken'; 
 
@@ -10,6 +10,7 @@ import { verifyToken } from '../middleware/verifyToken';
 userRoutes.get("/", findAllUsers);
 userRoutes.post("/", createUser);
 userRoutes.post("/authentication", authenticateUser);
+userRoutes.put("/update/:id", verifyToken, editUser);
 userRoutes.delete("/remove/:id", verifyToken, removeUsers);
 
 export { userRoutes };
