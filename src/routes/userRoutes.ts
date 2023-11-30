09aa2b997c1f3cf943e97117fb5a1cf8ec5f457c
Douglas Silva <douglassilvaaraujo78@gmail.com>
2023-11-29 23:25:33 -0300

    feat: Criando rota para o método PATH

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,15 +1,20 @@
 import express from 'express';
+import multer from 'multer';
 const userRoutes = express.Router();
 
 //Conjunto de requisições do usuário:
-import { findAllUsers, createUser, authenticateUser, removeUsers, editUser } from '../controllers/userControllers';
+import { findAllUsers, createUser, authenticateUser, removeUsers, editUser, uploadImage } from '../controllers/userControllers';
 //Importando o middleware de rerificação de token
 import { verifyToken } from '../middleware/verifyToken'; 
+//Importando o arquivo de configuração do multer
+import uploadConfigImage from '../config/multer';
+const upload = multer(uploadConfigImage); 
 
 //Rotas
 userRoutes.get("/", findAllUsers);
 userRoutes.post("/", createUser);
 userRoutes.post("/authentication", authenticateUser);
+userRoutes.patch("/upload", verifyToken, upload.single("image"), uploadImage);
 userRoutes.put("/update/:id", verifyToken, editUser);
 userRoutes.delete("/remove/:id", verifyToken, removeUsers);
 
