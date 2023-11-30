cbe3ffe9faef2a1ac5e788fbd34ef8c57d26f7ed
Douglas Silva <douglassilvaaraujo78@gmail.com>
2023-11-30 19:03:05 -0300

    refacto: Adiconando o middleware de images as rotas de remoção e edição

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -4,8 +4,10 @@ const userRoutes = express.Router();
 
 //Conjunto de requisições do usuário:
 import { findAllUsers, findSpecificUser, createUser, authenticateUser, removeUsers, editUser, uploadImage } from '../controllers/userControllers';
-//Importando o middleware de rerificação de token
+//Importando o middleware de verificação de token
 import { verifyToken } from '../middleware/verifyToken'; 
+//Importando o middleware do controle de upload de imagens
+import { verifyImageUser } from '../middleware/verifyImageUser';
 //Importando o arquivo de configuração do multer
 import uploadConfigImage from '../config/multer';
 const upload = multer(uploadConfigImage); 
@@ -14,9 +16,9 @@ const upload = multer(uploadConfigImage);
 userRoutes.get("/", findAllUsers);
 userRoutes.post("/", createUser);
 userRoutes.post("/authentication", authenticateUser);
-userRoutes.get("/:id", verifyToken, findSpecificUser);
-userRoutes.patch("/upload", verifyToken, upload.single("image"), uploadImage);
 userRoutes.put("/update/:id", verifyToken, editUser);
-userRoutes.delete("/remove/:id", verifyToken, removeUsers);
+userRoutes.get("/:id", verifyToken, findSpecificUser);
+userRoutes.delete("/remove/:id", verifyToken, removeUsers); //Quando deletar um usuário deve deletar a sua imagem  
+userRoutes.patch("/upload/:id", verifyToken, verifyImageUser, upload.single("image"), uploadImage); //Método de criar e editar a foto do usuário
 
 export { userRoutes };
