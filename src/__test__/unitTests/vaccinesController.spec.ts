03d0dfafa516fa4662209e017cc1e7d681f1c455
Jose Gabriel <jgabrielfd0@gmail.com>
2023-11-30 16:52:36 -0300

    feat: Criando um teste para a atualização de uma vacina

--- src/__test__/unitTests/vaccinesController.spec.ts
+++ src/__test__/unitTests/vaccinesController.spec.ts
@@ -3,12 +3,17 @@ const prismaMock = {
   vaccine: {
     findUnique: jest.fn(),
     create: jest.fn(),
+    update: jest.fn(),
   },
 };
 
 //Importando dependências
 import { Request, Response } from "express";
-import { createVaccines } from "../../controllers/vaccinesController";
+import {
+  createVaccines,
+  editVaccine,
+} from "../../controllers/vaccinesController";
+import { v4 as uuid } from "uuid";
 
 //Mokando o banco de dados
 jest.mock("../../prismaClient/prismaClient", () => ({
@@ -23,6 +28,7 @@ describe("Testando o fluxo normal da Api", () => {
 
   //Caso de teste 001 da rota vaccine
   it("Deve ser possível adicionar uma nova vacina", async () => {
+    // Criando o objeto request
     const req = {
       body: {
         name: "Vacina-test",
@@ -33,13 +39,65 @@ describe("Testando o fluxo normal da Api", () => {
       },
     } as Request;
 
+    // Criando o objeto response
     const res = {
       status: jest.fn().mockReturnThis(),
       json: jest.fn(),
     } as unknown as Response;
 
+    // Adicionando a vacina
     await createVaccines(req, res);
 
+    // Resultados esperado
     expect(res.status).toHaveBeenCalledWith(200);
+  });
+
+  it("Deve ser possívle editar uma vacina existente", async () => {
+    // Criando um id
+    const vaccineId: String = uuid();
+    prismaMock.vaccine.findUnique.mockResolvedValueOnce({
+      id: vaccineId,
+      name: "Vacina-test1",
+      type: "test1",
+      manufacturer: "by test1",
+      description: "Is a test1",
+      contraIndication: "not pass in test2.1",
+    });
+
+    // Criando o objeto request
+    const updateVaccine = {
+      name: "Vacina-test2",
+      type: "test2",
+      manufacturer: "by test2",
+      description: "Is a test2",
+      contraIndication: "not pass in test2",
+    };
+    const req = {
+      params: {
+        id: vaccineId,
+      },
+      body: updateVaccine,
+    } as unknown as Request;
+
+    // Criando o objeto response
+    const res = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    } as unknown as Response;
+
+    // Procedimento para atualizar a vacina
+    await editVaccine(req, res);
+
+    // Resultados esperados
+    expect(prismaMock.vaccine.update).toHaveBeenCalledWith({
+      data: updateVaccine,
+      where: {
+        id: vaccineId,
+      },
+    });
+    expect(res.status).toHaveBeenCalledWith(200);
+    expect(res.json).toHaveBeenCalledWith({
+      message: "Updated Vaccine",
+    });
   });
 });
