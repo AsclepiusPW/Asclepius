1af0e8d07a2bee56ab81fa4c58f31faea0e4b049
Marcos Paulo <kingzabitus@gmail.com>
2023-12-01 22:11:55 -0300

    feat: imprimir evento

--- src/__test__/unitTests/vaccineCalendarController.spec.ts
+++ src/__test__/unitTests/vaccineCalendarController.spec.ts
@@ -82,6 +82,113 @@ describe("Fluxo normal", () => {
             message: "Event removed",
         });
     });
+
+    //Caso de teste 003
+    it("Deve ser possível editar um evento já cadastrado", async () => {
+        // Supondo que exista um evento com esse id
+        const eventId = "026857bb-d5e9-4634-9170-2687a33f669e";
+        prismaMock.vaccinationCalendar.findUnique.mockResolvedValueOnce({
+            id: eventId,
+            local: 'Clínica Menino da Paz',
+            date: '2023-11-24T08:45:00.000Z',
+            places: 5,
+            responsible: 'Dr. Lucas',
+            status: 'Status not informed',
+            observation: 'Observation not informed',
+        });
+
+        // Construindo objeto de body
+        const updateEvent = {
+            local: 'Hostipal D. Pedro II',
+            date: '2023-11-24T08:45:00.000Z',
+            places: 50,
+            responsible: 'Dr. Lucas',
+        };
+
+        // Criando objeto request
+        const req = {
+            params: {
+                id: eventId,
+            },
+            body: updateEvent,
+        } as unknown as Request;
+
+        // Criando objeto response
+        const res = {
+            status: jest.fn().mockReturnThis(),
+            json: jest.fn(),
+        } as unknown as Response;
+
+        // Procedimento
+        await updateEventCalendar(req, res);
+
+        // Resultados esperados
+        expect(prismaMock.vaccinationCalendar.findUnique).toHaveBeenCalledWith({
+            where: { id: eventId },
+        });
+        // Verificando se a função findFirst foi chamada corretamente
+        expect(prismaMock.vaccinationCalendar.findFirst).toHaveBeenCalledWith({
+            where: {
+                date: '2023-11-24T08:45:00.000Z',
+                local: 'Hostipal D. Pedro II',
+            },
+        });
+        // Verificando se a função update foi chamada corretamente
+        expect(prismaMock.vaccinationCalendar.update).toHaveBeenCalledWith({
+            where: { id: eventId },
+            data: updateEvent,
+        });
+        // Verificando se a resposta está correta
+        expect(res.status).toHaveBeenCalledWith(200);
+        expect(res.json).toHaveBeenCalledWith({ message: "Update event" });
+    });
+
+    //Caso de teste 004
+    it("Deve ser possível imprimir as informações de um evento do calendário", async ()=>{
+        // Supondo que exista um evento com esse id
+    const calendarId = "026857bb-d5e9-4634-9170-2687a33f669e";
+    prismaMock.vaccinationCalendar.findUnique.mockResolvedValueOnce({
+      id: calendarId,
+      local: "Clínica Menino da Paz",
+      date: "2023-11-24T08:45:00.000Z",
+      places: 5,
+      responsible: "Dr. Lucas",
+      status: "Status not informed",
+      observation: "Observation not informed",
+      scheduleVaccine: [],
+    });
+
+    // Criando objeto request
+    const req = {
+      params: {
+        id: calendarId,
+      },
+    } as unknown as Request;
+
+    // Criando objeto response
+    const res = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    } as unknown as Response;
+
+    // Procedimento
+    await findSpecificCalendar(req, res);
+
+    // Resultados esperados
+    // Verificando se a resposta está correta
+    expect(res.status).toHaveBeenCalledWith(200);
+    expect(res.json).toHaveBeenCalledWith({
+      id: calendarId,
+      local: "Clínica Menino da Paz",
+      date: "2023-11-24T08:45:00.000Z",
+      places: 5,
+      responsible: "Dr. Lucas",
+      status: "Status not informed",
+      observation: "Observation not informed",
+      scheduleVaccine: [],
+    });
+    });
+
 });
 
 describe("Fluxo de exceções", () => {
