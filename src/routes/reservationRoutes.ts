e8a6e37f2c3848a3fa61d696a05cd6ad2984dfa6
Douglas Silva <douglassilvaaraujo78@gmail.com>
2023-12-04 14:40:33 -0300

    feat: Criando requisição e rota de remoção de uma solicitação de reserva

--- src/routes/reservationRoutes.ts
+++ src/routes/reservationRoutes.ts
@@ -2,11 +2,12 @@ import express from "express";
 const reservationRoutes = express.Router();
 
 //Importando as funções de vaccination
-import { requestReservation, listReservations } from "../controllers/reservationControllers";
+import { requestReservation, listReservations, removeReservation } from "../controllers/reservationControllers";
 import { verifyToken } from "../middleware/verifyToken";
 
 //Rotas
 reservationRoutes.get("/", verifyToken, listReservations);
 reservationRoutes.post("/", verifyToken, requestReservation);
+reservationRoutes.delete("/remove/:id", verifyToken, removeReservation);
 
 export { reservationRoutes };
