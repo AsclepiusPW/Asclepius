9112d87f2540887dcf98ec8d2bdd359b2971fb45
Douglas Silva <douglassilvaaraujo78@gmail.com>
2023-12-04 19:46:58 -0300

    feat: Criando rota e requisição de atualização das informações de uma reserva

--- src/routes/reservationRoutes.ts
+++ src/routes/reservationRoutes.ts
@@ -2,12 +2,13 @@ import express from "express";
 const reservationRoutes = express.Router();
 
 //Importando as funções de vaccination
-import { requestReservation, listReservations, removeReservation } from "../controllers/reservationControllers";
+import { requestReservation, listReservations, removeReservation, updateReservation } from "../controllers/reservationControllers";
 import { verifyToken } from "../middleware/verifyToken";
 
 //Rotas
 reservationRoutes.get("/", verifyToken, listReservations);
 reservationRoutes.post("/", verifyToken, requestReservation);
 reservationRoutes.delete("/remove/:id", verifyToken, removeReservation);
+reservationRoutes.put("/update/:id", verifyToken, updateReservation);
 
 export { reservationRoutes };
