43a92c04d3c875e698e1ee974cc8bc459ba889cf
Marcos Paulo <kingzabitus@gmail.com>
2023-12-05 22:07:29 -0300

    feat: listar solicitações

--- src/controllers/reservationControllers.ts
+++ src/controllers/reservationControllers.ts
@@ -71,3 +71,31 @@ export const requestReservation = async (req: Request, res: Response) => {
         return res.status(500).json({ error: "Internal Server Error" });
     }
 }
+
+
+//Método para listar todas as solicitações
+export const listReservations = async (req: Request, res: Response) => {
+    try {
+        //Pegando o id do usuário do tokne passado no headers da requisição
+        const userId = req.id_User;
+
+        //Validando a existência do usuário e anexando a propriedade Vaccination
+        const searchUser = await prisma.user.findUnique({
+            where: {
+                id: userId,
+            },
+            include: {
+                requestReservation: true,
+            }
+        });
+        if (!searchUser) {
+            return res.status(400).json({ error: "User not found" });
+        }
+
+        res.status(200).json(searchUser.requestReservation);
+    } catch (error) {
+        //Retornando erro caso haja
+        console.error("Error retrieving vaccination: ", error);
+        return res.status(500).json({ error: "Internal Server Error" });
+    }
+}
