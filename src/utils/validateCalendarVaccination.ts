662020caa542823a5e0d7b5a35992f206e81b66c
Douglas Silva <douglassilvaaraujo78@gmail.com>
2023-12-09 17:43:31 -0300

    add: Validação com Zod de Calendário de Vacinação

---
+++ src/utils/validateCalendarVaccination.ts
@@ -0,0 +1,11 @@
+import { z, ZodError } from "zod";
+
+export const calendarSchema = z.object({
+    local: z.string().min(3).max(255).refine(data => !!data, { message: 'The local is mandatory' }),
+    date: z.string().refine(data => !!data, { message: 'Invalid or missing date' }),
+    places: z.number().int().positive().refine(data => !!data, { message: 'The places is mandatory' }),
+    status: z.string().optional(),
+    observation: z.string().optional(),
+    responsible: z.string().min(1).refine(data => !!data, { message: 'The responsible is mandatory' }),
+    vaccine: z.string().refine(data => !!data, { message: 'The vaccine is mandatory' }),
+  });
