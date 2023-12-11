385239c01ba4a0a586632514e95ad265839cfcd6
Douglas Silva <douglassilvaaraujo78@gmail.com>
2023-12-11 19:05:00 -0300

    feat: Adicionando tratamento de erro ao middleware de usuário

--- src/middleware/verifyToken.ts
+++ src/middleware/verifyToken.ts
@@ -1,5 +1,5 @@
 import { Request, Response, NextFunction } from "express";
-import { verify } from "jsonwebtoken";
+import { verify, TokenExpiredError } from "jsonwebtoken";
 
 interface IPayload{
     sub: string,
@@ -26,7 +26,26 @@ export async function verifyToken(req:Request, res: Response, next: NextFunction
         req.id_User = sub;
     } catch (error) {
         console.error(error);
-        res.status(500).json({error: "Internal Server Error"});
+
+        try {
+            // Segunda verificação com SECRET
+            const userSecret = process.env.SECRET_ADMIN;
+            if (userSecret) {
+                const decodedUserToken = verify(token, userSecret);
+                // Se desejar, você pode acessar as informações do token decodificado usando decodedUserToken
+                return res.status(401).json({ message: "Restricted access - User only" });
+            } else {
+                throw new Error('JWT secret is not defined');
+            }
+        } catch (userError) {
+            console.error(userError);
+
+            if (userError instanceof TokenExpiredError) {
+                return res.status(401).json({ message: "Token expired" });
+            }
+
+            return res.status(401).json({ message: "Invalid token" });
+        }
     }
     next();
 }
